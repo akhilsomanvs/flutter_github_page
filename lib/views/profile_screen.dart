aaebf34488fe933df63a9567fae9030752902eca
akhilsomanvs <akhilsomanvs@gmail.com>
2021-11-09 21:26:59 +0530

    Launching URL to the design from dribbble added.

--- lib/views/profile_screen.dart
+++ lib/views/profile_screen.dart
@@ -4,6 +4,7 @@ import 'package:github_page/app_utils/app_theme.dart';
 import 'package:github_page/arch_utils/utils/size_config.dart';
 import 'package:github_page/arch_utils/widgets/responsive_safe_area.dart';
 import 'package:github_page/arch_utils/widgets/spacing_widgets.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class ProfileScreen extends StatelessWidget {
   ProfileScreen({Key? key}) : super(key: key);
@@ -34,23 +35,53 @@ class ProfileScreen extends StatelessWidget {
                     child: ColoredBox(
                       color: colorMainLight,
                       child: Padding(
-                        padding: EdgeInsets.all(paddingValue),
+                        padding: EdgeInsets.all(16),
                         child: Column(
                           mainAxisSize: MainAxisSize.max,
-                          mainAxisAlignment: MainAxisAlignment.center,
+                          mainAxisAlignment: MainAxisAlignment.start,
                           crossAxisAlignment: CrossAxisAlignment.start,
                           children: [
-                            Text("AKHIL", style: AppTheme.textTheme.headline4.copyWith(fontSize: 48.sp(), color: Colors.white, fontWeight: FontWeight.normal)),
-                            Text("SOMAN", style: AppTheme.textTheme.headline4.copyWith(fontSize: 48.sp(), color: Colors.white)),
-                            VSpace(48),
-                            Text("//region ABOUT ME", style: AppTheme.textTheme.bodyText1.copyWith(fontWeight: FontWeight.bold, color: Colors.white.withOpacity(0.6))),
-                            VSpace(32),
-                            Text(
-                              "I am mobile application developer with over 6 years of work experience. 5 Years in Android, 1.5 Years in Flutter.",
-                              style: AppTheme.textTheme.bodyText2.copyWith(/*fontWeight:FontWeight.bold,*/ color: Colors.white),
+                            Row(
+                              children: [
+                                Text("This design is from dribbble. ", style: AppTheme.textTheme.subtitle1.copyWith(fontWeight: FontWeight.normal, color: Colors.white.withOpacity(1))),
+                                MouseRegion(
+                                  cursor: SystemMouseCursors.click,
+                                  child: GestureDetector(
+                                    onTap: () async {
+                                      String designUrl = "https://dribbble.com/shots/16367386-ALEX-Freelancer-Portfolio-Web-Design";
+                                      if (await canLaunch(designUrl)) {
+                                        await launch(designUrl);
+                                      }
+                                    },
+                                    child: Text("Click here to see.",
+                                        style: AppTheme.textTheme.subtitle1.copyWith(fontWeight: FontWeight.normal, color: Colors.white, decoration: TextDecoration.underline)),
+                                  ),
+                                ),
+                              ],
                             ),
-                            VSpace(32),
-                            Text("//endregion ", style: AppTheme.textTheme.bodyText1.copyWith(color: Colors.white.withOpacity(0.6))),
+                            Expanded(
+                              child: Padding(
+                                padding: EdgeInsets.all(paddingValue),
+                                child: Column(
+                                  mainAxisSize: MainAxisSize.max,
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text("AKHIL", style: AppTheme.textTheme.headline4.copyWith(fontSize: 48.sp(), color: Colors.white, fontWeight: FontWeight.normal, height: 1)),
+                                    Text("SOMAN", style: AppTheme.textTheme.headline4.copyWith(fontSize: 48.sp(), color: Colors.white, height: 1)),
+                                    VSpace(48),
+                                    Text("//region ABOUT ME", style: AppTheme.textTheme.bodyText1.copyWith(fontWeight: FontWeight.normal, color: Colors.white.withOpacity(0.6))),
+                                    VSpace(32),
+                                    Text(
+                                      "I am mobile application developer with over 6 years of work experience. 5 Years in Android, 1.5 Years in Flutter.",
+                                      style: AppTheme.textTheme.bodyText2.copyWith(color: Colors.white),
+                                    ),
+                                    VSpace(32),
+                                    Text("//endregion ", style: AppTheme.textTheme.bodyText1.copyWith(fontWeight: FontWeight.normal, color: Colors.white.withOpacity(0.6))),
+                                  ],
+                                ),
+                              ),
+                            ),
                           ],
                         ),
                       ),
