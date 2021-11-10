43560909412ba0c0f5877d04eec3867a1aa37787
akhilsomanvs <akhilsomanvs@gmail.com>
2021-11-10 19:17:55 +0530

    Added responsive class to show different layout in different screen sizes.

--- lib/app_utils/app_colours.dart
+++ lib/app_utils/app_colours.dart
@@ -4,3 +4,4 @@ Color colorPageBackground = const Color(0xFFECBDA6);
 Color colorMainLight = const Color(0xFFFE8F72);
 Color colorMainDark = const Color(0xFFFF734D);
 Color colorFooter = const Color(0xFF282634);
+Color colorGreyText = const Color(0xFF7F7D86);
--- lib/views/profile_screen.dart
+++ lib/views/profile_screen.dart
@@ -2,14 +2,16 @@ import 'package:flutter/material.dart';
 import 'package:github_page/app_utils/app_colours.dart';
 import 'package:github_page/app_utils/app_theme.dart';
 import 'package:github_page/arch_utils/utils/size_config.dart';
+import 'package:github_page/arch_utils/widgets/responsive.dart';
 import 'package:github_page/arch_utils/widgets/responsive_safe_area.dart';
 import 'package:github_page/arch_utils/widgets/spacing_widgets.dart';
+import 'package:github_page/views/components/landing_page.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 class ProfileScreen extends StatelessWidget {
   ProfileScreen({Key? key}) : super(key: key);
 
-  double _screenWidth = 0;
+  double _usableWidth = 0;
   double _usableHeight = 0;
 
   @override
@@ -19,84 +21,18 @@ class ProfileScreen extends StatelessWidget {
       body: ResponsiveSafeArea(
         builder: (BuildContext context, Size size) {
           _usableHeight = size.height;
-          _screenWidth = size.width;
+          _usableWidth = size.width;
           double paddingValue = 60;
+          double heightAfterPadding = _usableHeight - (2 * paddingValue);
+          debugPrint("WIDTH ::::: $_usableHeight __ $_usableWidth __ $heightAfterPadding");
           return Container(
-            height: _usableHeight,
             padding: EdgeInsets.all(paddingValue),
             decoration: BoxDecoration(
               color: colorPageBackground,
             ),
-            child: Row(
-              children: [
-                Expanded(
-                  child: SizedBox(
-                    height: _usableHeight - (2 * paddingValue),
-                    child: ColoredBox(
-                      color: colorMainLight,
-                      child: Padding(
-                        padding: EdgeInsets.all(16),
-                        child: Column(
-                          mainAxisSize: MainAxisSize.max,
-                          mainAxisAlignment: MainAxisAlignment.start,
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Row(
-                              children: [
-                                Text("This design is from dribbble. ", style: AppTheme.textTheme.subtitle1.copyWith(fontWeight: FontWeight.normal, color: Colors.white.withOpacity(1))),
-                                MouseRegion(
-                                  cursor: SystemMouseCursors.click,
-                                  child: GestureDetector(
-                                    onTap: () async {
-                                      String designUrl = "https://dribbble.com/shots/16367386-ALEX-Freelancer-Portfolio-Web-Design";
-                                      if (await canLaunch(designUrl)) {
-                                        await launch(designUrl);
-                                      }
-                                    },
-                                    child: Text("Click here to see.",
-                                        style: AppTheme.textTheme.subtitle1.copyWith(fontWeight: FontWeight.normal, color: Colors.white, decoration: TextDecoration.underline)),
-                                  ),
-                                ),
-                              ],
-                            ),
-                            Expanded(
-                              child: Padding(
-                                padding: EdgeInsets.all(paddingValue),
-                                child: Column(
-                                  mainAxisSize: MainAxisSize.max,
-                                  mainAxisAlignment: MainAxisAlignment.center,
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: [
-                                    Text("AKHIL", style: AppTheme.textTheme.headline4.copyWith(fontSize: 48.sp(), color: Colors.white, fontWeight: FontWeight.normal, height: 1)),
-                                    Text("SOMAN", style: AppTheme.textTheme.headline4.copyWith(fontSize: 48.sp(), color: Colors.white, height: 1)),
-                                    VSpace(48),
-                                    Text("//region ABOUT ME", style: AppTheme.textTheme.bodyText1.copyWith(fontWeight: FontWeight.normal, color: Colors.white.withOpacity(0.6))),
-                                    VSpace(32),
-                                    Text(
-                                      "I am mobile application developer with over 6 years of work experience. 5 Years in Android, 1.5 Years in Flutter.",
-                                      style: AppTheme.textTheme.bodyText2.copyWith(color: Colors.white),
-                                    ),
-                                    VSpace(32),
-                                    Text("//endregion ", style: AppTheme.textTheme.bodyText1.copyWith(fontWeight: FontWeight.normal, color: Colors.white.withOpacity(0.6))),
-                                  ],
-                                ),
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                    ),
-                  ),
-                ),
-                Expanded(
-                  child: SizedBox(
-                    height: _usableHeight - (2 * paddingValue),
-                    child: ColoredBox(
-                      color: colorMainDark,
-                    ),
-                  ),
-                ),
-              ],
+            child: Responsive(
+              desktop: LandingPageDesktop(),
+              mobile: LandingPageMobile(),
             ),
           );
         },
